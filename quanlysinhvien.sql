aeb80659011431bd79005e01b53242a51dcc805a
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-12-14 19:45:53 +0700

    sqldesign

---
+++ quanlysinhvien.sql
@@ -0,0 +1,9 @@
+USE quanlysinhvien;class
+CREATE TABLE Class(
+ClassID INT AUTO_INCREMENT PRIMARY KEY,
+ClassName VARCHAR (60) NOT Null,
+StartDate DATETIME NOT NULL,
+STATUS bit
+)
+CREATE TABLE Student(
+
